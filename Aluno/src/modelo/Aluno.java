cadbd91ccb4b125eafc10e0a08d4e877a056ee79
Administrador <Administrador@10.10.4.12>
2019-06-27 15:48:09 -0300

    huuhfud

---
+++ Aluno/src/modelo/Aluno.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String endereço;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    
+    
+    
+}
